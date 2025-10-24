a3314946e153851c5c82f6530b8e71b0ba62198d
ESTEBAN ROVERI <esteban.roveri@iut.local>
2025-10-24 08:12:52 +0200

    Init tp socket

---
+++ Socket/Serveur.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+
+public class Serveur {
+    public static void main(String[] args) throws Exception {
+        // 1. Création du serveur et liaison au port 5000
+
+        while(true) {
+            // 2. Acceptation d'une connexion cliente
+
+            // 3. Création des canaux input/output
+
+            // 4. Boucle d'écho (lecture/écriture)
+
+            // 5. Fermeture de la connexion client
+        }
+    }
+}
